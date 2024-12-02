8738895cf0dd56001b58d1c3140c85f6f1f01948
Paint <AHMED00AHMED00GAMAL@gmail.com>
2024-12-02 17:00:43 +0200

    replace location manager with fused location provider.

--- app/src/main/java/com/ag/projects/aatask/presentation/fragments/prayer_times/PrayerTimesFragment.kt
+++ app/src/main/java/com/ag/projects/aatask/presentation/fragments/prayer_times/PrayerTimesFragment.kt
@@ -1,10 +1,6 @@
 package com.ag.projects.aatask.presentation.fragments.prayer_times
 
-import android.content.Context
 import android.content.pm.PackageManager
-import android.location.Location
-import android.location.LocationListener
-import android.location.LocationManager
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
@@ -36,14 +32,13 @@ import kotlinx.coroutines.launch
 import java.util.Calendar
 
 @AndroidEntryPoint
-class PrayerTimesFragment : Fragment(), LocationListener {
+class PrayerTimesFragment : Fragment() {
 
     private lateinit var binding: FragmentPrayerTimesBinding
 
     private val viewModel: PrayerTimeFragmentViewModel by viewModels()
 
     private lateinit var requestPermissionLauncher: ActivityResultLauncher<Array<String>>
-    private lateinit var locationManager: LocationManager
 
     private lateinit var fusedLocationClient: FusedLocationProviderClient
     private var currentPosition = 0
@@ -69,12 +64,11 @@ class PrayerTimesFragment : Fragment(), LocationListener {
 
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(requireContext())
 
-        locationManager =
-            requireActivity().getSystemService(Context.LOCATION_SERVICE) as LocationManager
         initBinding()
         setupPrayerTimesRV()
         checkLocationPermission()
 
+        getUserLocation()
         fetchLocalData()
 
     }
@@ -203,14 +197,6 @@ class PrayerTimesFragment : Fragment(), LocationListener {
             return
         }
 
-        locationManager.requestLocationUpdates(
-            LocationManager.GPS_PROVIDER,
-            5000L,
-            10f,
-            this
-        )
-
-        /*
          fusedLocationClient.lastLocation.addOnSuccessListener { location ->
 
              location?.let {
@@ -245,35 +231,7 @@ class PrayerTimesFragment : Fragment(), LocationListener {
              Log.d("prayer fragment location is ", "Canceled")
              return@addOnCanceledListener
          }
-         */
     }
-
-    override fun onLocationChanged(location: Location) {
-        latitude = location.latitude
-        longitude = location.longitude
-
-        if (latitude != 0.0 && longitude != 0.0) {
-            Log.d("InsertPrayerTimesUseCase", "lat is $latitude and lng is $longitude")
-            val userLocation =
-                getAddressFromLatLng(
-                    requireContext(),
-                    location.latitude,
-                    location.longitude
-                )
-
-            binding.tvUserLocation.text = userLocation
-            getPrayerTimes()
-        }
-    }
-
-    override fun onProviderDisabled(provider: String) {
-        super.onProviderDisabled(provider)
-        Toast.makeText(
-            requireContext(),
-            getString(R.string.enable_location_services), Toast.LENGTH_SHORT
-        ).show()
-    }
-
     private fun fetchLocalData() {
         lifecycleScope.launch {
             if (viewModel.localDataExist()) {
